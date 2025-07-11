5e34cf5a02d68d3415c6a9083a0b75cace75d40e
sachin_sde <ssachin.s@myclnq.co.in>
2025-07-11 16:17:40 +0530

    sample

--- src/Hooks/UseMemo/Memo.jsx
+++ src/Hooks/UseMemo/Memo.jsx
@@ -2,20 +2,23 @@ import React, { useMemo, useState } from 'react'
 
 const Memo = () => {
     const [count, setCount] = useState(0);
-    const [number, setNumber] = useState(0);
-
-    const expensiveCalculation =(num)=>{
-        console.log("Expensive cal");
-        for(let i=0;i<1000000000;i++){}
-            return num * 2;
+    const [number,setNumber] = useState(0);
+    const expensiveCal = (num) =>{
+        console.log("Inside cal")
+        for(let i=0;i<10000000;i++){}
+        return num*2;
     }
-    const doubleValue = useMemo(()=>expensiveCalculation(number),[number])
+    const doubleResult = useMemo(() => expensiveCal(number),[number])
   return (
     <div>
-          <h1>{doubleValue}</h1>
-          <input type="number" value={number} onChange={(e) =>setNumber(e.target.value)} />
-       <h1>{count}</h1>
-       <button onClick={()=>setCount(count+1)}>+</button>
+        <h1>{count}</h1>
+        <button onClick={()=>setCount(count+1)}>+</button>
+
+        <div>
+            <h2>{doubleResult}</h2>
+            <input type="number" value={number} onChange={(e)=>setNumber(e.target.value)} />
+        </div>
+      
     </div>
   )
 }
