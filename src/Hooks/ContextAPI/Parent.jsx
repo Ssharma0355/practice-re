7dba4d5b468886bb6883594bb06bdc541b0133bc
sachin_sde <ssachin.s@myclnq.co.in>
2025-07-11 12:54:01 +0530

    useContext

--- src/Hooks/ContextAPI/Parent.jsx
+++ src/Hooks/ContextAPI/Parent.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React, { createContext, useState } from 'react'
+import Comp1 from './Comp1';
 
+
+const UserContext = createContext();
 const Parent = () => {
+    const [user, setUser] = useState({
+        name:"Sachin Sharma",
+        email:"ssharma0355@gmail.com",
+        job:"SDE"
+    })
   return (
-    <div>
-      
-    </div>
+      <UserContext.Provider value={user}>
+        <Comp1 />
+      </UserContext.Provider>
   )
 }
 
-export default Parent
+export default Parent;
+export { UserContext  }
