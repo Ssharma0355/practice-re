3d5b6d9521cf2b71a2a5e86f4ced35af0d8c572b
sachin_sde <ssachin.s@myclnq.co.in>
2025-07-12 09:11:33 +0530

    callback 1st case done

--- src/Hooks/UseCallback/Callback.jsx
+++ src/Hooks/UseCallback/Callback.jsx
@@ -1,12 +1,16 @@
-import React, { useState } from 'react'
+import React, { useCallback, useState } from 'react'
 import Child11 from './Child11';
 // 2 cases - unnessacry rednder of child component save
 const Callback = () => {
     const [counter, setCounter] = useState(0);
-    const handleClick =()=>{
-        setCounter(counter+1)
-
-    }
+    //when re-render then function refrence re-creates 
+    // const handleClick =()=>{
+    //     setCounter(counter+1)
+    // }
+    const handleClick = useCallback(()=>{
+        setCounter(counter + 1)
+    },[]); 
+    //function is freezee
   return (
     <div>
         <h1>{counter}</h1>
